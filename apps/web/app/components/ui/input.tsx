a56d9c0aee232c8fa8d06712de60366665170fb2
nycx <eyronick@gmail.com>
2026-04-17 02:48:42 +0530

    chore: input box type and some package version mismatch

--- apps/web/app/components/ui/input.tsx
+++ apps/web/app/components/ui/input.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Input as InputPrimitive } from "@base-ui/react/input";
 import { cn } from "../../lib/utils";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+type InputProps = Omit<InputPrimitive.Props, "ref">;
+
+function Input({ className, type, ...props }: InputProps) {
   return (
     <InputPrimitive
       type={type}
